dffb7926b06449d68b63526b99c8e8319372cf32
Dishita <ds3864@drexel.edu>
2026-02-17 11:49:37 -0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holiday_management',
+    version='0.1.0',
+    author="Dishita Singh",
+    author_email="dishita.singh@yahoo.com",
+    packages=find_packages()
+)
